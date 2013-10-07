f0e597033519ab28b48f9df6366839d66f49f328
David Arvelo <david@davidarvelo.com>
2013-10-06 21:59:51 -0400

    Set up initial controllers, minus ItemController

---
+++ app/scripts/app/controllers/Groups.js
@@ -0,0 +1,13 @@
+define([
+  'ember'
+],
+
+function (Em) {
+  'use strict';
+
+  var GroupsController = Em.ArrayController.extend({
+
+  });
+
+  return GroupsController;
+});
---
+++ app/scripts/app/controllers/Items.js
@@ -0,0 +1,13 @@
+define([
+  'ember'
+],
+
+function (Em) {
+  'use strict';
+
+  var ItemsController = Em.ObjectController.extend({
+
+  });
+
+  return ItemsController;
+});
