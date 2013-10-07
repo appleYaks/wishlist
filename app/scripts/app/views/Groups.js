c199c276046d46d7ac58183ce50c3e9923860da4
David Arvelo <david@davidarvelo.com>
2013-10-06 22:00:09 -0400

    Set up initial views, minus ItemView

---
+++ app/scripts/app/views/Groups.js
@@ -0,0 +1,14 @@
+define([
+  'ember',
+  'JST/groups',
+],
+
+function (Em) {
+  'use strict';
+
+  var GroupsView = Em.View.extend({
+
+  });
+
+  return GroupsView;
+});
---
+++ app/scripts/app/views/Items.js
@@ -0,0 +1,14 @@
+define([
+  'ember',
+  'JST/items',
+],
+
+function (Em) {
+  'use strict';
+
+  var ItemsView = Em.View.extend({
+
+  });
+
+  return ItemsView;
+});
