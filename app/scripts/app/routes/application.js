58dce022ddda69f2ea347f1f5b324204a2d20975
David Arvelo <david@davidarvelo.com>
2014-05-30 23:47:28 -0400

    Remove outdated bootstrap code in ApplicationRoute

--- app/scripts/app/routes/application.js
+++ app/scripts/app/routes/application.js
@@ -1,24 +1,3 @@
-var ApplicationRoute = Em.Route.extend({
-  model: function () {
-    // bootstrap initialized data if it exists
-    if (Em.isArray(window.bootstrap)) {
-      this.store.pushMany('person', window.bootstrap);
-
-      // signal to IndexController that data exists
-      // and fetching from the server is not necessary
-      // IndexRoute will remove this property after its first render.
-      return Em.Object.create({
-        bootstrapped: true,
-      });
-    }
-
-    // signal to IndexController that data does not yet exist
-    // and fetching from the server is necessary.
-    // IndexRoute will remove this property after its first render.
-    return Em.Object.create({
-      bootstrapped: false
-    });
-  },
-});
+var ApplicationRoute = Em.Route.extend();
 
 export default ApplicationRoute;
