e0d67de504742e157c1652c6cdb60713a4c4ec1f
David Arvelo <david@davidarvelo.com>
2014-05-31 23:29:30 -0400

    Remove experimental fluff from ApplicationView, add classname

--- app/scripts/app/views/application.js
+++ app/scripts/app/views/application.js
@@ -1,9 +1,5 @@
 var ApplicationView = Em.View.extend({
-  eventManager: {
-    doubleClick: function (e, view) {
-      console.log('*** ApplicationView Event: double-click event on view: ', view, ', Event: ', e);
-    },
-  },
+  classNames: ['application'],
 });
 
 export default ApplicationView;
