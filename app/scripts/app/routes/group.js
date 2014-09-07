4337f62cfd7a7bad3ca1378736d9092ef76a83d8
David Arvelo <david@davidarvelo.com>
2014-09-06 23:44:49 -0400

    Remove some boilerplate and Ember hints from grunt-express-workflow

--- app/scripts/app/routes/group.js
+++ app/scripts/app/routes/group.js
@@ -1,19 +1,4 @@
 var GroupRoute = Ember.Route.extend({
-  // beforeModel: function () {
-  //   console.log('moving into group route')
-  // },
-
-  // afterModel: function (model, transition) {
-  //   console.log('transition aborted?', transition.isAborted, transition)
-  //   // transition.abort();
-  //   // // this.transitionTo('groups')
-  //   // console.log('transition aborted now?', transition.isAborted, transition)
-  //   // return
-  //   return Ember.RSVP.Promise(function (resolve) {
-  //     Ember.run.later(function() { resolve(model); }, 1000);
-  //   });
-  // },
-
   model: function (params) {
     return this.store.find('group', params.group_id);
   },
--- app/scripts/app/routes/groups.js
+++ app/scripts/app/routes/groups.js
@@ -1,19 +1,4 @@
 var GroupsRoute = Em.Route.extend({
-  actions: {
-    willTransition: function (transition) {
-      // console.log('yo son')
-      // transition.abort();
-      // var promise = Ember.RSVP.Promise(function (resolve) {
-      //   setTimeout(function () {
-      //     transition.retry();
-      //     resolve();
-      //   }, 3000);
-      // });
-      // return promise;
-    },
-
-  },
-
   model: function () {
     var preload = this.get('preload');
 
