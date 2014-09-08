33e9a265c78aec7c57a7b8ffe2974b00ae5dab97
David Arvelo <david@davidarvelo.com>
2014-09-08 16:58:58 -0400

    Remove unused code to create a pojo from an Ember object

--- app/scripts/misc/config.js
+++ app/scripts/misc/config.js
@@ -8,33 +8,3 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   );
   document.getElementsByTagName('head')[0].appendChild(msViewportStyle);
 }
-
-// might not be necessary since Ember seems to serialize nested Ember Objects just fine on REST...
-function plainObject (EmberObject) {
-  var EmO = Em.Object;
-  var isArray = Em.isArray;
-  var keys = Em.keys(EmberObject);
-  var pojo = EmberObject.getProperties(keys);
-  var prop;
-
-  // check keys for nested Ember.Objects
-  for (var key in pojo) {
-    prop = pojo[key];
-
-    // check for Ember.Objects
-    if (prop instanceof EmO) {
-      pojo[key] = plainObject(prop);
-    }
-
-    // check arrays for Ember.Objects
-    if (isArray(prop)) {
-      pojo[key] = prop = prop.slice();
-
-      for (var i = prop.length - 1; i >= 0; i--) {
-        if (prop[i] instanceof EmO) {
-          prop[i] = plainObject(prop[i]);
-        }
-      }
-    }
-  }
-}
