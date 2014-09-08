2ab29798a3acf6f1f0513099753bd1b788c8387a
David Arvelo <david@davidarvelo.com>
2014-09-08 17:37:33 -0400

    Prune modules from Gruntfile, bower.json, and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,12 @@
 // use this if you want to match all subfolders:
 // 'test/spec/**/*.js'
 
+var path = require('path'),
+    susy = path.join(__dirname, 'bower_components/susy/sass/'),
+    breakpoint = path.join(__dirname, 'bower_components/breakpoint-sass/stylesheets/'),
+    fontAwesome = path.join(__dirname, 'bower_components/font-awesome/scss'),
+    normalize = path.join(__dirname, 'bower_components/normalize-scss/');
+
 module.exports = function (grunt) {
     // show elapsed time at the end
     require('time-grunt')(grunt);
@@ -50,7 +56,7 @@ module.exports = function (grunt) {
         watch: {
             compass: {
                 files: ['<%= yeoman.app %>/styles/**/*.{scss,sass}'],
-                tasks: ['compass:server'],
+                tasks: ['sass:dev'],
             },
             emberTemplates: {
                 files: [
@@ -126,8 +132,29 @@ module.exports = function (grunt) {
                 }
             }
         },
-
+        sass: {
+            options: {
+                loadPath: [susy, breakpoint, normalize, fontAwesome],
+                compass: true,
+                require: [],
+            },
+            dev: {
+                files: {
+                    '.tmp/styles/main.css': 'app/styles/main.scss',
+                }
+            },
+            dist: {
+                options: {
+                    sourcemap: false,
+                    style: 'compressed',
+                },
+                files: {
+                    'dist/styles/main.css': 'app/styles/main.scss',
+                }
+            }
+        },
         clean: {
+            server: ['.tmp', '.sass-cache'],
             dist: {
                 files: [{
                     dot: true,
@@ -138,15 +165,6 @@ module.exports = function (grunt) {
                         '!<%= yeoman.dist %>/.git*'
                     ]
                 }]
-            },
-            server: ['.tmp', '.sass-cache'],
-            compassSprites: {
-                files: [{
-                    src: [
-                        '<%= yeoman.dist %>/<%= yeoman.app %>/images/sprites/*',
-                        '!<%= yeoman.dist %>/<%= yeoman.app %>/images/sprites/*.*',
-                    ],
-                }],
             },
             transpiled: {
                 files: [{
@@ -168,55 +186,7 @@ module.exports = function (grunt) {
         //         'test/spec/**/*.js'
         //     ]
         // },
-        // mocha: {
-        //     all: {
-        //         options: {
-        //             run: true,
-        //             // urls: ['http://localhost:<%= connect.options.port %>/index.html']
-        //         }
-        //     }
-        // },
-        compass: {
-            options: {
-                sassDir: '<%= yeoman.app %>/styles',
-                cssDir: '.tmp/styles',
-                imagesDir: '<%= yeoman.app %>/images',
-                generatedImagesDir: '.tmp/images',
-                fontsDir: '<%= yeoman.app %>/styles/fonts',
-                javascriptsDir: '<%= yeoman.app %>/scripts',
-                importPath: 'bower_components',
 
-                // advanced compass config necessary for spritemaps
-                // https://gist.github.com/passy/5270050
-                // https://github.com/yeoman/yeoman/issues/419
-                httpImagesPath: '/images',
-                httpGeneratedImagesPath: '/images',
-                httpFontsPath: '/styles/fonts',
-                // let the grunt-rev task handle cache-busting
-                assetCacheBuster: false,
-                // `relativeAssets: true` messes up spritemap url() references in CSS
-                relativeAssets: false,
-                // this can house any extra settings we might
-                // need not provided by grunt-contrib-compass.
-                // cannot be combined with these options:
-                //   * generatedImagesDir
-                //   * httpImagesPath
-                //   * httpGeneratedImagesPath
-                //   * httpFontsPath
-                //   * assetCacheBuster
-                // config: 'compass.rb',
-            },
-            dist: {
-                options: {
-                    generatedImagesDir: '<%= yeoman.dist %>/<%= yeoman.app %>/images'
-                }
-            },
-            server: {
-                options: {
-                    debugInfo: true,
-                }
-            }
-        },
         // Uglify task does concat,
         // but still available if needed
         concat: {
@@ -225,16 +195,14 @@ module.exports = function (grunt) {
                     '.tmp/scripts/vendor.js': [
                         'bower_components/loader.js/loader.js',
                         'bower_components/jquery/dist/jquery.js',
-                        'bower_components/jquery-ui/ui/jquery-ui.js',
                         'bower_components/hammerjs/dist/jquery.hammer.js',
                         'bower_components/handlebars/handlebars.js',
-                        'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.js',
                         'bower_components/ember/ember.js',
                         'bower_components/ember-resolver/dist/ember-resolver.js',
                         'bower_components/ember-load-initializers/ember-load-initializers.js',
                         'bower_components/validator-js/validator.js',
                         'bower_components/moment/moment.js',
-                        'app/scripts/misc/config.js',
+                        'app/scripts/misc/ms-device-width.js',
                     ]
                 }
             }
@@ -252,89 +220,6 @@ module.exports = function (grunt) {
             }
         },
 
-        rev: {
-            dist: {
-                files: {
-                    src: [
-                        '<%= yeoman.dist %>/<%= yeoman.app %>/scripts/**/*.js',
-                        '<%= yeoman.dist %>/<%= yeoman.app %>/styles/**/*.css',
-                        '<%= yeoman.dist %>/<%= yeoman.app %>/images/**/*.{png,jpg,jpeg,gif,webp}',
-                        '<%= yeoman.dist %>/<%= yeoman.app %>/styles/fonts/**/*.*'
-                    ]
-                }
-            }
-        },
-        useminPrepare: {
-            options: {
-                dest: '<%= yeoman.dist %>/<%= yeoman.app %>'
-            },
-            html: ['<%= yeoman.app %>/{,*/}*.html', 'views/**/*.jade']
-        },
-        usemin: {
-            options: {
-                dirs: ['<%= yeoman.dist %>/<%= yeoman.app %>'],
-                basedir: '<%= yeoman.dist %>/<%= yeoman.app %>',
-            },
-            html: ['<%= yeoman.dist %>/<%= yeoman.app %>/{,*/}*.html', '<%= yeoman.dist %>/views/**/*.jade'],
-            css: ['<%= yeoman.dist %>/<%= yeoman.app %>/styles/{,*/}*.css']
-        },
-       imagemin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= yeoman.app %>/images',
-                    src: '**/*.{png,jpg,jpeg}',
-                    dest: '<%= yeoman.dist %>/<%= yeoman.app %>/images'
-                }]
-            }
-        },
-        svgmin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= yeoman.app %>/images',
-                    src: '{,*/}*.svg',
-                    dest: '<%= yeoman.dist %>/<%= yeoman.app %>/images'
-                }]
-            }
-        },
-        cssmin: {
-            // This task is pre-configured if you do not wish to use Usemin
-            // blocks for your CSS. By default, the Usemin block from your
-            // `index.html` will take care of minification, e.g.
-            //
-            //     <!-- build:css({.tmp,app}) styles/main.css -->
-            //
-            // dist: {
-            //     files: {
-            //         '<%= yeoman.dist %>/<%= yeoman.app %>/styles/main.css': [
-            //             '.tmp/styles/{,*/}*.css',
-            //             '<%= yeoman.app %>/styles/{,*/}*.css'
-            //         ]
-            //     }
-            // }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    /*removeCommentsFromCDATA: true,
-                    // https://github.com/yeoman/grunt-usemin/issues/44
-                    //collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true*/
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= yeoman.app %>',
-                    src: '*.html',
-                    dest: '<%= yeoman.dist %>/<%= yeoman.app %>'
-                }]
-            }
-        },
         // Put files not handled in other tasks here
         copy: {
             dist: {
@@ -358,191 +243,25 @@ module.exports = function (grunt) {
                     src: '**/*.jade',
                 }]
             },
-            styles: {
-                expand: true,
-                dot: true,
-                cwd: '<%= yeoman.app %>/styles',
-                dest: '.tmp/styles/',
-                src: '{,*/}*.css'
-            },
-        },
-        modernizr: {
-            devFile: 'bower_components/modernizr/modernizr.js',
-            outputFile: '<%= yeoman.dist %>/<%= yeoman.app %>/scripts/vendor/modernizr.js',
-            files: [
-                '<%= yeoman.dist %>/<%= yeoman.app %>/scripts/{,*/}*.js',
-                '<%= yeoman.dist %>/<%= yeoman.app %>/styles/{,*/}*.css',
-                '!<%= yeoman.dist %>/<%= yeoman.app %>/scripts/vendor/*'
-            ],
-            uglify: true
         },
         concurrent: {
-            nodeInspector: {
-                options: {
-                    logConcurrentOutput: true,
-                },
-                tasks: [
-                    'nodemon:nodeInspector',
-                    'watch'
-                ]
-            },
-            nodemon: {
-                options: {
-                    logConcurrentOutput: true,
-                },
-                tasks: [
-                    'nodemon:nodeInspector',
-                    'nodemon:dev',
-                    'watch',
-                ],
-            },
             server: [
-                // 'coffee:dist',
-                'compass:server',
-                'copy:styles',
-                // 'handlebars:app',
+                'sass:dev',
                 'emberTemplates:app',
                 'concat:dev-ember',
                 'concat_sourcemap:client',
             ],
             // test: [
-                // 'coffee',
-                // 'compass',
-                // 'handlebars:app',
             // ],
             dist: [
-                // 'coffee',
-                // 'handlebars:app',
                 'transpile',
                 'emberTemplates:app',
-                'copy:styles',
                 'compass:dist',
                 'svgmin',
-                'htmlmin'
+                'htmlmin',
+                'sass:dist',
             ]
         },
-        bower: {
-            options: {
-                exclude: ['modernizr']
-            },
-            all: {
-                rjsConfig: '<%= yeoman.app %>/scripts/main.js'
-            }
-        },
-        // handlebars: {
-        //     app: {
-        //         options: {
-        //             namespace: false,
-        //             amd: true,
-        //             processContent: function(content) {
-        //                 content = content.replace(/^[\x20\t]+/mg, '').replace(/[\x20\t]+$/mg, '');
-        //                 content = content.replace(/^[\r\n]+/, '').replace(/[\r\n]*$/, '\n');
-        //                 return content;
-        //             }
-        //         },
-        //         files: [{
-        //             expand: true,
-        //             cwd: '<%= yeoman.app %>/scripts/app/templates/',
-        //             src: ['**/*.hbs'],
-        //             dest: '.tmp/scripts/app/templates/',
-        //             ext: '.js',
-        //         }],
-        //     },
-        // },
-        nodemon: {
-            dev: {
-                script: 'server.js',
-                options: {
-                    args: ['development', karmaConfig.app.port],
-                    nodeArgs: ['--debug'],
-                    // ext: 'js',
-                    ext: 'dontRestart',
-                    // nodemon watches the current directory recursively by default
-                    // watch: ['.'], // default
-                    delay: 1,
-                    ignore: nodemonIgnoredFiles,
-                }
-            },
-            nodeInspector: {
-                script: 'node-inspector.js',
-                options: {
-                    // ext: 'js',
-                    ext: 'dontRestart',
-                    ignore: nodemonIgnoredFiles,
-                    exec: 'node-inspector',
-                    delay: 3
-                },
-            },
-        },
-        karma: {
-            options: {
-                // configure browsers that will work for you.
-                // it's also possible to specify scripts/binaries that will take a URL argument:
-                // browsers: ['/usr/bin/firefox'],
-
-                // Currently available:
-                // - Chrome
-                // - ChromeCanary
-                // - Firefox
-                // - Opera
-                // - Safari (only Mac)
-                // - PhantomJS
-                // - IE (only Windows)
-                browsers: ['PhantomJS'],
-                // run karma in a child process so it doesn't block subsequent grunt tasks.
-                background: true,
-            },
-            // NoCoverage tasks are slightly faster
-            // Continous tasks are used for a single run, such as during a build
-
-            // `karma` task settings for testing the namespaced client-side app
-            app: {
-                configFile: karmaConfig.app.configFile,
-                port: karmaConfig.app.port,
-
-                reporters: ['dots', 'coverage'],
-                coverageReporter: {
-                    type: 'html',
-                    dir: 'coverage/frontend/app/',
-                },
-                preprocessors: {
-                    '**/app/scripts/app/**/*.js': 'coverage',
-                },
-            },
-            appNoCoverage: {
-                configFile: karmaConfig.app.configFile,
-                port: karmaConfig.app.port,
-
-                reporters: ['dots'],
-            },
-            appContinuous: {
-                configFile: karmaConfig.app.configFile,
-                port: karmaConfig.app.port,
-
-                reporters: ['dots', 'coverage'],
-                coverageReporter: {
-                    type: 'html',
-                    dir: 'coverage/frontend/app/',
-                },
-                preprocessors: {
-                    '**/app/scripts/app/**/*.js': 'coverage',
-                },
-
-                background: false,
-                singleRun: true,
-            },
-            appContinuousNoCoverage: {
-                configFile: karmaConfig.app.configFile,
-                port: karmaConfig.app.port,
-
-                reporters: ['dots'],
-
-                background: false,
-                singleRun: true,
-            },
-        },
-        // simplemocha executes server-side tests
-        // without Istanbul Coverage, significantly faster
         simplemocha: {
             options: {
                 globals: [
@@ -608,107 +327,31 @@ module.exports = function (grunt) {
         },
     });
 
-    grunt.registerTask('coverageBackend', 'Test backend files as well as code coverage.', function () {
-        var done = this.async();
-
-        var path = './test/backend/runner.js';
-
-        var options = {
-            cmd: 'istanbul',
-            grunt: false,
-            args: [
-                'cover',
-                '--default-excludes',
-                '-x', 'app/**',
-                '--report', 'lcov',
-                '--dir', './coverage/backend',
-                path
-            ],
-            opts: {
-                // preserve colors for stdout in terminal
-                stdio: 'inherit',
-            },
-        };
-
-        function doneFunction(error, result, code) {
-            if (result && result.stderr) {
-                process.stderr.write(result.stderr);
-            }
-
-            if (result && result.stdout) {
-                grunt.log.writeln(result.stdout);
-            }
-
-            // abort tasks in queue if there's an error
-            done(error);
-        }
-
-        grunt.util.spawn(options, doneFunction);
-    });
-
     grunt.registerTask('server', [
         'clean:server',
         'transpile',
         'concurrent:server',
-
-        // start karma server
-        // 'karma:app',
-
         'express:dev',
         'watch',
-        // 'concurrent:nodeInspector',
     ]);
 
     grunt.registerTask('test', [
+        // 'jshint',
         'clean:server',
         'transpile',
         'concurrent:server',
-
-        // tests with coverage.
-        // the server-side coverage test is slower
-        'karma:appContinuous',
-        'coverageBackend',
-
-        // faster tests without Istanbul coverage
-        // 'karma:appContinuousNoCoverage',
-        // 'simplemocha:backend',
     ]);
 
     grunt.registerTask('build', [
         'clean:dist',
         'concurrent:dist',
-
-        'karma:appContinuous',
-        'coverageBackend',
-
-        'useminPrepare',
-
-        // Any other requirejs 'sub-projects' can be
-        // compiled with 'requirejs:subprojectName'
-        'requirejs:app',
-
-        // place after compass:dist in order to
-        // ensure compass spritemaps are generated
-        // before images are copied over to dist/
-        'imagemin',
-
         'concat',
-        'cssmin',
         'uglify',
-        'modernizr',
         'copy:dist',
-
-        'rev',
-        'usemin',
-
-        // remove sprites folders in dist/images/sprites/
-        // in favor of having just the spritesheet files
-        'clean:compassSprites',
+        'sass:dist',
     ]);
 
     grunt.registerTask('default', [
-        // 'jshint',
-        // 'test',
-        'build'
+        'server'
     ]);
 };
--- compass.rb
+++
@@ -1,2 +0,0 @@
-# This file can include some additonal compass configuration settings
-# If you use it, make sure to activate it in the `compass` task of Gruntfile.js
