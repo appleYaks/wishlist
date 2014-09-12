c3d67f14d83dd107e8aa43c97a3de5742ab9b5b0
David Arvelo <david@davidarvelo.com>
2014-09-11 23:54:58 -0400

    Move livereload port back to normal (switched machines)

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
                 ],
                 options: {
                     livereload: {
-                        port: 1337
+                        port: 35729
                     },
                 },
             },
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
                 ],
                 options: {
                     livereload: {
-                        port: 1337
+                        port: 35729
                     }
                 },
             },
