179ad2f9e6e9eb15d71c5643d13809c3578b25f1
David Arvelo <david@davidarvelo.com>
2014-09-11 23:59:11 -0400

    Fetch item from API on a refresh to the item route

--- app/scripts/app/routes/item.js
+++ app/scripts/app/routes/item.js
@@ -1,4 +1,8 @@
 var ItemRoute = Em.Route.extend({
+  model: function (params) {
+    return $.getJSON('/api/v1/items/' + params.item_id);
+  },
+
   renderTemplate: function () {
     this.render('item', {
       into: 'application',
