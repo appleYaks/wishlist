91fb13c2ca810911c1ecd16ab249fc37e2aeed51
David Arvelo <david@davidarvelo.com>
2014-09-13 02:38:56 -0400

    Add model types to DataStore after instantiation

--- app/scripts/app/initializers/inject-data-store.js
+++ app/scripts/app/initializers/inject-data-store.js
@@ -1,10 +1,18 @@
 import DataStore from 'client/utils/data-store';
 
 export default {
-    name: 'datastore',
+    name: 'data-store',
 
     initialize: function (container, application) {
+      var store;
+
       application.register('store:main', DataStore);
       application.inject('route', 'store', 'store:main');
+
+      // add basic model types to the store so they can accept models
+      store = container.lookup('store:main');
+      store.addType('groups');
+      store.addType('items');
+      window.s = store;
     }
 };
