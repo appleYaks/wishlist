178db6041626973df57ef2c8c0d4ddcab79a1651
David Arvelo <david@davidarvelo.com>
2014-09-16 19:11:12 -0400

    Move preload test to homepage

--- test/frontend/data-store.js
+++ test/frontend/data-store.js
@@ -1,43 +1,18 @@
 describe('DataStore', function () {
+  var store;
+
   beforeEach(function () {
     visit('/');
+    store = App.__container__.lookup('store:main');
   });
 
   afterEach(function () {
+    store = null;
     App.reset();
   });
 
-  it('should push preloaded data into the store', function() {
-    var now = new Date();
-    var groups = [{
-        id: 999,
-        title: 'test group',
-        order: 0,
-        date: now,
-        fields: '[]'
-    }];
-
-    // attach GroupModel preload data
-    var meta = document.createElement('meta');
-    meta.setAttribute('name', 'preload-groups');
-    meta.setAttribute('content', JSON.stringify(groups));
-    document.head.appendChild(meta);
-
-    // restart the app so it can get the preload
-    Ember.run(function () {
-      App.reset();
-    });
-
-    document.head.removeChild(meta);
-
-    andThen(function () {
-      var store = App.__container__.lookup('store:main'),
-          testGroup = store.findById('groups', 999);
-
-      assertDataStoreCount('groups', 1);
-
-      expect(testGroup).to.exist;
-      testGroup.title.should.equal(groups[0].title);
-    });
+  it('should ', function () {
+    expect(1);
+    console.log(store);
   });
 });
