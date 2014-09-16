c40900b5793ae62b3b6747f5c4f95e077acc636f
David Arvelo <david@davidarvelo.com>
2014-09-16 19:26:41 -0400

    Update parseFields to handle `date` property

--- app/scripts/app/utils/parse-fields-metadata.js
+++ app/scripts/app/utils/parse-fields-metadata.js
@@ -1,11 +1,25 @@
+var fields = ['fields', 'date'];
+
 function parseFields (payload) {
+  var i;
+
   if (Array.isArray(payload)) {
     payload.forEach(parseFields);
     return;
   }
 
-  if (typeof payload === 'object' && payload.hasOwnProperty('fields')) {
-    payload.fields = JSON.parse(payload.fields);
+  if (typeof payload !== 'object') {
+    return;
+  }
+
+  for (i = 0; i !== fields.length; ++i) {
+    if (payload.hasOwnProperty(fields[i])) {
+      if (fields[i] === 'date') {
+        payload[fields[i]] = new Date(payload[fields[i]]);
+      } else {
+        payload[fields[i]] = JSON.parse(payload[fields[i]]);
+      }
+    }
   }
 }
 
