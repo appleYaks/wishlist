6d92f99a8a8581ff7456f569570cb14e5a31c55d
David Arvelo <david@davidarvelo.com>
2014-09-23 23:50:39 -0400

    Create temp Item for ItemEdit route unless edits are canceled

--- app/scripts/app/routes/item/edit.js
+++ app/scripts/app/routes/item/edit.js
@@ -1,4 +1,18 @@
 var ItemEditRoute = Em.Route.extend({
+  setupController: function (controller, item) {
+    var tempItem = this.generateTempItem(item);
+    tempItem = Ember.Object.create(tempItem);
+
+    controller.set('model', tempItem);
+    controller.set('canonicalModel', item);
+  },
+
+  // deep copy of passed-in model to mess with in case edits are canceled
+  // seems like `.toJSON()` is not supported for `Ember.Object`
+  generateTempItem: function (item) {
+    return JSON.parse(JSON.stringify(item));
+  },
+
   renderTemplate: function () {
     this.render('item/edit', {
       into: 'application',
