3224c411fe305d83a76c476315860b26502abd27
David Arvelo <david@davidarvelo.com>
2014-09-23 21:42:58 -0400

    Add 'Edit Item' button

--- app/scripts/app/controllers/items.js
+++ app/scripts/app/controllers/items.js
@@ -1,11 +1,15 @@
 var ItemsController = Em.ArrayController.extend({
   actions: {
-    deleteItem: function (item) {
+    delete: function (item) {
       console.log('deleting item: ', item);
       // send API DELETE request with model's `id` and `GroupId`
       // tell `controllers.items` to slice out this model from its content
       // this.get('controllers.items');
     },
+
+    edit: function (item) {
+      this.transitionToRoute('item.edit', item);
+    }
   }
 });
 
