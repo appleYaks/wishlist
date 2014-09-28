ba95c6ff0e5be1fdb7b2c0ebf4a395f7923a590d
David Arvelo <david@davidarvelo.com>
2014-09-27 23:26:53 -0400

    Group and Item model factories registered on the DataStore

--- app/scripts/app/initializers/inject-data-store.js
+++ app/scripts/app/initializers/inject-data-store.js
@@ -1,4 +1,6 @@
 import DataStore from 'client/utils/data-store';
+import GroupModel from 'client/models/group';
+import ItemModel from 'client/models/item';
 
 export default {
     name: 'data-store',
@@ -12,6 +14,8 @@ export default {
       // add basic model types to the store so they can accept models
       store = container.lookup('store:main');
       store.addType('groups');
+      store.registerModel('groups', GroupModel);
       store.addType('items');
+      store.registerModel('items', ItemModel);
     }
 };
