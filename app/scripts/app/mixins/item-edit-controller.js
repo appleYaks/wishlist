d73fb4a368bdc9d77a845e07379c82982d6ca438
David Arvelo <david@davidarvelo.com>
2014-10-01 18:11:36 -0400

    Allow booleanType on a new field in ItemEdicControllerMixin

--- app/scripts/app/mixins/item-edit-controller.js
+++ app/scripts/app/mixins/item-edit-controller.js
@@ -1,11 +1,13 @@
-var stringType = { type: 'String', string: true };
-var numberType = { type: 'Number', number: true };
-var dateType   = { type: 'Date', date: true };
+var stringType  = { type: 'String', string: true };
+var numberType  = { type: 'Number', number: true };
+var booleanType = { type: 'Boolean', bool: true };
+var dateType    = { type: 'Date', date: true };
 
 var ItemEditControllerMixin = Ember.Mixin.create({
   fieldKeyTypes: [
     stringType,
     numberType,
+    booleanType,
     dateType
   ],
 
