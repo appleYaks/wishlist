90aaabc6abc8783b5fc91d53be0b8692a1f6e492
David Arvelo <david@davidarvelo.com>
2014-10-04 14:14:16 -0400

    Update the testing bootstrap implementation

--- test/frontend/homepage.js
+++ test/frontend/homepage.js
@@ -8,16 +8,18 @@ describe('homepage', function () {
   });
 
   it('redirects to /groups', function () {
-    expect(currentURL()).to.equal('/groups');
+    findWithAssert('.content');
+    andThen(function () {
+      expect(currentURL()).to.equal('/groups');
+      findWithAssert('.groups');
+    });
   });
 
   it('pushes preloaded data into the store', function() {
-    var now = new Date();
     var groups = [{
         id: 999,
         title: 'test group',
         order: 0,
-        date: now,
         fields: '[]'
     }];
 
@@ -28,9 +30,7 @@ describe('homepage', function () {
     document.head.appendChild(meta);
 
     // restart the app so it can get the preload
-    Ember.run(function () {
-      App.reset();
-    });
+    App.reset();
 
     document.head.removeChild(meta);
 
