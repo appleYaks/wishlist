f985ce70407549898a6123965841107cee6afbe2
David Arvelo <david@davidarvelo.com>
2014-11-04 22:39:43 -0500

    Add loading spinner

--- app/scripts/app/routes/application.js
+++ app/scripts/app/routes/application.js
@@ -1,3 +1,31 @@
-var ApplicationRoute = Em.Route.extend();
+var body = document.body;
+
+var ApplicationRoute = Em.Route.extend({
+  spinnerEl: 'div',
+  spinnerClassNames: 'loading fa fa-spin fa-spinner',
+
+  removeLoadingSpinner: function () {
+    var spinnerClassNames = this.get('spinnerClassNames'),
+        spinnerEl = this.get('spinnerEl'),
+        spinners = [].slice.call(body.children).filter(function (el) {
+          return el.className === spinnerClassNames && el.nodeName.toLowerCase() === spinnerEl;
+        });
+
+    spinners.forEach(function (spinner) {
+      body.removeChild(spinner);
+    });
+  },
+
+  actions: {
+    loading: function () {
+      var spinnerEl = this.get('spinnerEl'),
+          spinner = document.createElement(spinnerEl);
+
+      spinner.className = this.get('spinnerClassNames');
+      body.appendChild(spinner);
+      this.router.on('didTransition', this, 'removeLoadingSpinner');
+    }
+  }
+});
 
 export default ApplicationRoute;
