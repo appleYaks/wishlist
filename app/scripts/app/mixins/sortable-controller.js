d2499a3066fba5b14040f55d50fa33376f361184
David Arvelo <david@davidarvelo.com>
2014-11-05 23:50:40 -0500

    Allow clearing a sorting method

--- app/scripts/app/mixins/sortable-controller.js
+++ app/scripts/app/mixins/sortable-controller.js
@@ -8,6 +8,16 @@ var SortControllerMixin = Ember.Mixin.create({
 
     return which;
   }.property('sortProperties', 'sortAscending'),
+
+  actions: {
+    clearSort: function () {
+      // the default from the data store is store ascending by id.
+      // if we set both to null, and the previous vals were to sort anything descending,
+      // the list will be sorted by id, but in descending order.
+      this.set('sortProperties', ['id']);
+      this.set('sortAscending', true);
+    }
+  }
 });
 
 export default SortControllerMixin;
